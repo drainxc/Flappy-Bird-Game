9d33b113d680d5ddcd5efcdf5ba58917b040bac6
ldh7228 <ldh7228@gmail.com>
2021-07-30 16:40:59 +0900

    Add bird fall event

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ let bird = new Image();
 let pipe = new Image();
 let birdX = 100;
 let birdY = 100;
+let gravity = 5;
 
 bird.src = "../asset/bird.png";
 pipe.src = "../asset/pipe.png";
 
-bird.onload = function(){ 
-    ctx.drawImage(bird, birdX, birdY); 
-}
+setInterval(function () {
+    ctx.clearRect(0, 0, 1520, 699);
+    ctx.drawImage(bird, birdX, birdY);
+    birdY += gravity;
+}, 20);
