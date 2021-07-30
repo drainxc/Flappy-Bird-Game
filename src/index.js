23a720a730819f971918b0e25e4cffb797fbf9af
ldh7228 <ldh7228@gmail.com>
2021-07-30 16:31:25 +0900

    Add draw bird event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let bird = new Image();
+let pipe = new Image();
+let birdX = 100;
+let birdY = 100;
+
+bird.src = "../asset/bird.png";
+pipe.src = "../asset/pipe.png";
+
+bird.onload = function(){ 
+    ctx.drawImage(bird, birdX, birdY); 
+}
